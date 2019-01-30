77e91fd2a0dc2ba3eebd0e642fafef0aedae7ce2
ritusingla <41640484+ritusingla@users.noreply.github.com>
2019-01-30 17:52:16 +0530

    Add files via upload

---
+++ final_website/complete.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{    int n;
+     printf("enter the oeder of matrix:");
+     scanf("%d",&n);
+	int mat1[n][n];
+	inti;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&mat1);
+	}
+	/*determinant of matrix*/
+	int j;
+	for(i=0,j=0)
+}
---
+++ final_website/homeframes.h
@@ -0,0 +1,16 @@
+<html>
+<head>
+<frameset rows="15%,85%">
+<frame src="a.htm"/>
+<frameset cols="15%,60%,25%">
+<frame src="b.htm"/>
+<frame src="home.html"/>
+<frame src="d.htm"/>
+</frameset>
+</frameset>
+<title>FRAMESET</title>
+</head>
+<body onload="alert('popform.html')">
+<img src="bgimage.jpg"/>
+</body>
+</html>
